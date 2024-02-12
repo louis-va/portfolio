fdaa8691467b451980733f85771a208545148bb2
Louis Van Aken <vanakenlouis@gmail.com>
2024-02-12 11:45:34 +0100

    feat: add navigation in project pages

---
+++ src/utils/array.utils.ts
@@ -0,0 +1,21 @@
+function getAdjacentElements(array: any[], index: number) {
+  const length = array.length;
+  if (length === 0) {
+      return [undefined, undefined];
+  }
+  
+  let prevIndex = index - 1;
+  let nextIndex = index + 1;
+  
+  if (index === 0) {
+      prevIndex = length - 1; // second to last element
+  } else if (index === length - 1) {
+      nextIndex = 0; // second element
+  }
+
+  console.table([index, prevIndex, nextIndex]);
+  
+  return [array[prevIndex], array[nextIndex]];
+}
+
+export { getAdjacentElements };
